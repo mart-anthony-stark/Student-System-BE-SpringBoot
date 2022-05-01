998d24d26409b8da86405d505a31a6f590e916c9
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-01 13:10:25 +0800

    Add: save and get students

--- src/main/java/com/martanthony/studentsystem/service/StudentServiceImplementation.java
+++ src/main/java/com/martanthony/studentsystem/service/StudentServiceImplementation.java
@@ -1,5 +1,29 @@
 package com.martanthony.studentsystem.service;
 
-public class StudentServiceImplementation {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.martanthony.studentsystem.model.Student;
+import com.martanthony.studentsystem.repository.StudentRepository;
+
+@Service
+public class StudentServiceImplementation implements StudentService{
+	
+	@Autowired
+	private StudentRepository studentRepository;
+	
+	@Override
+	public Student saveStudent(Student student) {
+		// TODO Auto-generated method stub
+		studentRepository.save(student);
+		return null;
+	}
+
+	@Override
+	public List<Student> getStudents() {
+		// TODO Auto-generated method stub
+		return studentRepository.findAll();
+	}
 }
