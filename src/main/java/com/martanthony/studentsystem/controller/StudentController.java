5384079834472954e9c7a5e5d4c6e519fe77ede6
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-01 13:09:09 +0800

    Add: get and post mapping

--- src/main/java/com/martanthony/studentsystem/controller/StudentController.java
+++ src/main/java/com/martanthony/studentsystem/controller/StudentController.java
@@ -1,5 +1,31 @@
 package com.martanthony.studentsystem.controller;
 
-public class StudentController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.martanthony.studentsystem.model.Student;
+import com.martanthony.studentsystem.service.StudentService;
+
+@RestController
+@RequestMapping("/student")
+public class StudentController {
+	@Autowired
+	private StudentService studentService;
+	
+	@GetMapping("")
+	public List<Student> get(){
+		return studentService.getStudents();
+	}
+	
+	@PostMapping("/add")
+	public Student add(@RequestBody Student student) {
+		studentService.saveStudent(student);
+		return student;
+	}
 }
