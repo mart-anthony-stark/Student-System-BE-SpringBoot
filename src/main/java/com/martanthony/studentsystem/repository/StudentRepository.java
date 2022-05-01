062191848ec42d0db0364251bebf2116105ee2f4
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-01 13:49:27 +0800

    Add: findByName

--- src/main/java/com/martanthony/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/martanthony/studentsystem/repository/StudentRepository.java
@@ -1,10 +1,12 @@
 package com.martanthony.studentsystem.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.martanthony.studentsystem.model.Student;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Integer> {
-
+	List<Student> findByName(String name);
 }
