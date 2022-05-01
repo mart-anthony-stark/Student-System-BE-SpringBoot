d3efc3ef453d7d734d0db279ee3bd5c75a1a5c25
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-01 14:34:55 +0800

    Rmove: unused imports

--- src/main/java/com/martanthony/studentsystem/repository/StudentRepository.java
+++ src/main/java/com/martanthony/studentsystem/repository/StudentRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import com.martanthony.studentsystem.model.Student;
 
 @Repository
