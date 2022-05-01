eab9602f2a728bccc921d9ed99c3be192a720c26
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-01 14:34:38 +0800

    Add: delete and update route

--- src/main/java/com/martanthony/studentsystem/controller/StudentController.java
+++ src/main/java/com/martanthony/studentsystem/controller/StudentController.java
@@ -1,10 +1,14 @@
 package com.martanthony.studentsystem.controller;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,13 +25,28 @@ public class StudentController {
 	private StudentService studentService;
 	
 	@GetMapping("")
+	@ResponseBody
 	public List<Student> get(@RequestParam(required=false) String name){
 		return studentService.getStudents(name);
 	}
 	
-	@PostMapping("/add")
+	@PostMapping("")
+	@ResponseBody
 	public Student add(@RequestBody Student student) {
 		studentService.saveStudent(student);
+		return student;
+	}
+	
+	@DeleteMapping("/{id}")
+	@ResponseBody
+	public HashMap<String, Boolean> delete(@PathVariable int id) {
+		return studentService.deleteOne(id);
+	}
+	
+	@PutMapping("")
+	@ResponseBody
+	public Student update(@RequestBody Student student) {
+		studentService.updateOne(student);
 		return student;
 	}
 }
