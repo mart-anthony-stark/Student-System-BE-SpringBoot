320d38a42ce4707ec140e3eb5f82f705e8925e09
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-01 14:35:08 +0800

    Add: delete and update abstraction

--- src/main/java/com/martanthony/studentsystem/service/StudentService.java
+++ src/main/java/com/martanthony/studentsystem/service/StudentService.java
@@ -1,5 +1,6 @@
 package com.martanthony.studentsystem.service;
 
+import java.util.HashMap;
 import java.util.List;
 
 import com.martanthony.studentsystem.model.Student;
@@ -8,4 +9,6 @@ public interface StudentService {
 	public Student saveStudent(Student student);
 	public List<Student> getStudents(String name);
 	public Student getOne(int id);
+	public HashMap<String, Boolean> deleteOne(int id);
+	public Student updateOne(Student student);
 }
